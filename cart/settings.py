4b2265dc245c18018447bc489f00a67a745d754c
Navya <amnavya91@gmail.com>
2026-02-12 20:38:18 +0530

    cloudinary change

--- cart/settings.py
+++ cart/settings.py
@@ -42,15 +42,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'cloudinary',
+    'cloudinary_storage',
     'customers',
     'orders',
     'themes',
 ]
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME', 'dmtghys2n'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY', '675474162936152'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
