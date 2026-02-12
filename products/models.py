e00ee4d97a30de1d1ea596435bea5c78f8b8ff05
Navya <amnavya91@gmail.com>
2026-02-12 15:08:52 +0530

    model change

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='media/products/', blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     stock = models.PositiveIntegerField(default=0)
     priority=models.PositiveIntegerField(default=0)
     deleted_at=models.IntegerField(choices=DELETE_CHOICE)
