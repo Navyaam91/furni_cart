c576be4af56672a512e43e49723fbd32ad3ee2a8
Navya <amnavya91@gmail.com>
2026-02-14 15:52:37 +0530

    admin.py

--- customers/admin.py
+++ customers/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Customer
-
+from .models import Contact 
 admin.site.register(Customer)
+
+
+@admin.register(Contact) # Update this too
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'created_at')
