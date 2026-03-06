6cc21f21327bb1fb3663bc1f7cb55fd9d3e4e1fc
Navya <amnavya91@gmail.com>
2026-03-06 10:35:16 +0530

    product model changes

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from customers.models import Customer
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     priority = models.PositiveIntegerField(default=0)
@@ -11,26 +11,42 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
-    LIVE=1
-    DELETE=0
-    DELETE_CHOICE=((LIVE,'Live'),(DELETE,'Delete'))
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products')
+
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICE = ((LIVE, 'Live'), (DELETE, 'Delete'))
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='products'
+    )
+
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    # image = models.ImageField(upload_to='products/', blank=True, null=True)
-    image = CloudinaryField('image', blank=True, null=True),
+
+    image = CloudinaryField('image', blank=True, null=True)
+
     stock = models.PositiveIntegerField(default=0)
-    seller = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='products_sold', null=True, blank=True)
-    priority=models.PositiveIntegerField(default=0)
-    deleted_at=models.IntegerField(choices=DELETE_CHOICE)
+
+    seller = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+        related_name='products_sold',
+        null=True,
+        blank=True
+    )
+
+    priority = models.PositiveIntegerField(default=0)
+    deleted_at = models.IntegerField(choices=DELETE_CHOICE)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
@@ -39,4 +55,4 @@ class Review(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.customer.firstname} - {self.product.name} - {self.rating}"
+        return self.name
